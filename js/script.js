d4f53da9ab352a7fd942301b6e0fe367238ce4ab
GiovanniCalia <johnca@outlook.it>
2022-03-04 16:00:56 +0100

    Creazione in pagina HTML

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
-//let randomNumbers = document.getElementById("numeri-random");
+let randomNumber = document.getElementById("numeri-random");
 setTimeout(indovinaNumero, 3000);
-let arrNumbers = []
+let arrNumbers = [];
+let userNumbers = [];
 
 
 let i = 0
 
 while ( i < 5){
-    randomNumbers = Math.floor(Math.random() * (100 - 1) + 1);
-    console.log(randomNumbers);
+    let randomNumbers = Math.floor(Math.random() * (100 - 1) + 1);
+    const eleNumber = document.createElement("div");
+    eleNumber.classList.add("number");
+    eleNumber.innerHTML = randomNumbers;
+    randomNumber.append(eleNumber);
     arrNumbers.push(randomNumbers);
     i++
+    console.log(arrNumbers)
 }
 
 function indovinaNumero(){
@@ -19,11 +24,21 @@ function indovinaNumero(){
 
         if (arrNumbers.includes(tellNumbers)){
             console.log("ottimo!");
+            userNumbers.push(tellNumbers)
         } else{
             console.log("non va bene");
         }
-    }   
+        //console.log(userNumbers) 
+    }
+    if (arrNumbers = userNumbers){
+        console.log("Hai vinto!");
+    } else {
+        console.log("Hai perso!");
+    } 
+    console.log(arrNumbers)
+    console.log(userNumbers)
 }
+
 
 
 
