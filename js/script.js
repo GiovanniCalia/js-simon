59edc13804873806447894ce2affd576cc1302c1
GiovanniCalia <johnca@outlook.it>
2022-03-04 14:47:57 +0100

    Continuazione dell'esercizio

--- js/script.js
+++ js/script.js
@@ -1,15 +1,33 @@
-const tellNumbers = parseInt(prompt("Reinserisci i numeri in ordine"));
-console.log(tellNumbers);
+//let randomNumbers = document.getElementById("numeri-random");
+setTimeout(indovinaNumero, 3000);
+let arrNumbers = []
 
-
-setTimeout(indovinaNumero, 30000);
 
 let i = 0
 
 while ( i < 5){
-    let randomNumbers = Math.floor(Math.random() * (max - min) + min);
+    randomNumbers = Math.floor(Math.random() * (100 - 1) + 1);
     console.log(randomNumbers);
+    arrNumbers.push(randomNumbers);
     i++
 }
+
+function indovinaNumero(){
+    for (i = 0; i < 5; i++){
+        const tellNumbers = parseInt(prompt("Reinserisci i numeri in ordine"));
+        console.log(tellNumbers);
+
+        if (arrNumbers.includes(tellNumbers)){
+            console.log("ottimo!");
+        } else{
+            console.log("non va bene");
+        }
+    }   
+}
+
+
+
+
+
 
 
