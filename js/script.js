63ac02d3e77ff1ba93803e93cce0bf6d815b4df3
GiovanniCalia <johnca@outlook.it>
2022-03-05 19:16:07 +0100

    Funzionamento della sparizione dei numeri

--- js/script.js
+++ js/script.js
@@ -4,7 +4,56 @@ let arrNumbers = [];
 let userNumbers = [];
 let i = 0
 
-setTimeout(numeriDnone, 1000);
+while ( i < 5){
+    let randomNumbers = Math.floor(Math.random() * (100 - 1) + 1);
+    while (arrNumbers.includes(randomNumbers)){
+        randomNumbers = Math.floor(Math.random() * (100 - 1) + 1);
+    }
+    arrNumbers.push(randomNumbers);
+    console.log(randomNumbers);
+    const eleNumber = document.createElement("div");
+    eleNumber.classList.add("number");
+    eleNumber.innerHTML = randomNumbers;
+    randomNumber.append(eleNumber);
+    i++;
+} 
+
+setTimeout(clearNumbers, 2990);
+
+function clearNumbers(){
+    randomNumber.style.display = "none";
+}
+
+setTimeout(indovinaNumero, 3000);
+
+function indovinaNumero(){
+    for (i = 0; i < 5; i++){
+        let tellNumbers = parseInt(prompt("Reinserisci i numeri in ordine"));
+        console.log(tellNumbers);
+        userNumbers.push(tellNumbers);
+        
+        /*
+        if (arrNumbers.includes(tellNumbers)){
+            console.log("ottimo!");
+            userNumbers.push(tellNumbers)
+        } else{
+            console.log("non va bene");
+        }*/
+    }
+    if (arrNumbers == userNumbers){
+        result.innerHTML = `Hai vinto!`;
+    } else {
+        result.innerHTML = `Hai perso!`;
+    } 
+    console.log(arrNumbers);
+    console.log(userNumbers);
+
+}
+
+
+
+
+
 
 /*
 while ( i < 5){
@@ -17,55 +66,3 @@ while ( i < 5){
     i++
     console.log(arrNumbers)
 }*/
-
-function numeriDnone(){
-    while ( i < 5){
-        let randomNumbers = Math.floor(Math.random() * (100 - 1) + 1);
-        while (arrNumbers.includes(randomNumbers)){
-            randomNumbers = Math.floor(Math.random() * (100 - 1) + 1);
-        }
-        arrNumbers.push(randomNumbers);
-        console.log(randomNumbers);
-        const eleNumber = document.createElement("div");
-        eleNumber.classList.add("number");
-        eleNumber.innerHTML = randomNumbers;
-        randomNumber.append(eleNumber);
-        i++
-        console.log(arrNumbers)
-    }
-}
-
-setTimeout(indovinaNumero, 3000);
-
-function indovinaNumero(){
-    randomNumber.style.display = "none";
-    for (i = 0; i < 5; i++){
-        let tellNumbers = parseInt(prompt("Reinserisci i numeri in ordine"));
-        console.log(tellNumbers);
-        userNumbers.push(tellNumbers)
-        
-        if (arrNumbers.includes(tellNumbers)){
-            console.log("ottimo!");
-            userNumbers.push(tellNumbers)
-        } else{
-            console.log("non va bene");
-        }
-        //console.log(userNumbers) 
-        console.log(arrNumbers)
-        console.log(userNumbers)
-    }
-    if (arrNumbers == userNumbers){
-        result.innerHTML = `Hai vinto!`;
-    } else {
-        result.innerHTML = `Hai perso!`;
-    } 
-
-}
-
-
-
-
-
-
-
-
