4e546fd92bb28ccaaf557ca0beb81165006b8d5d
GiovanniCalia <johnca@outlook.it>
2022-03-06 22:17:49 +0100

    Completamento dell'esercizio

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 let randomNumber = document.getElementById("numeri-random");
 let result = document.getElementById("risultato");
 let arrNumbers = [];
-let userNumbers = [];
+let exactNumbers = [];
 let i = 0
 
 while ( i < 5){
     let randomNumbers = Math.floor(Math.random() * (100 - 1) + 1);
+
     while (arrNumbers.includes(randomNumbers)){
         randomNumbers = Math.floor(Math.random() * (100 - 1) + 1);
     }
+    
     arrNumbers.push(randomNumbers);
     console.log(randomNumbers);
     const eleNumber = document.createElement("div");
@@ -27,46 +29,14 @@ function clearNumbers(){
 setTimeout(indovinaNumero, 3000);
 
 function indovinaNumero(){
-    let numeriesatti = [];
+
     for (i = 0; i < 5; i++){
         let tellNumbers = parseInt(prompt("Reinserisci i numeri in ordine"));
-        //console.log(tellNumbers);
-        userNumbers.push(tellNumbers);
-        
-        
+
         if (arrNumbers.includes(tellNumbers)){
-            numeriesatti.push(tellNumbers);
-            console.log("ottimo!");
-            userNumbers.push(tellNumbers)
-        } else{
-            console.log("non va bene");
+            exactNumbers.push(tellNumbers);
         }
     }
-    console.log(`hai indovinato questi numeri: ${numeriesatti}`);
-    /*
-    if (arrNumbers.includes(numeriesatti)){
-        result.innerHTML = `Hai vinto!`;
-    } else {
-        result.innerHTML = `Hai perso!`;
-    } 
-    console.log(arrNumbers);
-    console.log(userNumbers);
-    console.log(numeriesatti)*/
+
+    result.innerHTML = (`Hai indovinato questi numeri: ${exactNumbers}.`);
 }
-
-
-
-
-
-
-/*
-while ( i < 5){
-    let randomNumbers = Math.floor(Math.random() * (100 - 1) + 1);
-    const eleNumber = document.createElement("div");
-    eleNumber.classList.add("number");
-    eleNumber.innerHTML = randomNumbers;
-    randomNumber.append(eleNumber);
-    arrNumbers.push(randomNumbers);
-    i++
-    console.log(arrNumbers)
-}*/
